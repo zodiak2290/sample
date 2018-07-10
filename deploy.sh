b2a0a2dd667fd44ac3a02d6a41094e51dfc2d7ff
Hector Ortiz <hector@reservamos.mx>
2018-07-10 15:27:14 -0500

    Add script deploy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+-mkdir me
+cd me
+-rm dist/public/assets/css/*
+-rmdir dist/public/assets/css/
+-rm dist/public/assets/imgs/*
+-rmdir dist/public/assets/imgs/
+-rm dist/public/assets/*
+-rmdir dist/public/assets/
+-rm dist/public/*
+-rmdir dist/public/
+-rm dist/*
+-rmdir dist/
+put package.json
+put ecosystem.config.json
+-mkdir dist/
+put -r dist/
